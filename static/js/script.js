89189a03a4c2e564ab56379018fbcbd98a628369
WhizzyDoc <hassanridwan2536@gmail.com>
2023-09-22 17:03:59 +0100

    initial commit

---
+++ static/js/script.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+    $('.opennav').click(function() {
+        $('.nav').addClass('open');
+    })
+    $('.closenav').click(function() {
+        $('.nav').removeClass('open');
+    })
+})
